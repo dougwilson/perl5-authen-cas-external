ade4b2efcd407d954af1ccffcfb438ed62d4780f
Douglas Christopher Wilson <doug@somethingdoug.com>
2009-04-18 00:06:31 -0400

    Initial module creation using Module::Starter

---
+++ lib/Authen/CAS/External.pm
@@ -0,0 +1,107 @@
+package Authen::CAS::External;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Authen::CAS::External - The great new Authen::CAS::External!
+
+=head1 VERSION
+
+Version 0.01_01
+
+=cut
+
+our $VERSION = '0.01_01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Authen::CAS::External;
+
+    my $foo = Authen::CAS::External->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Douglas Christopher Wilson, C<< <doug at somethingdoug.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-authen-cas-external at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Authen-CAS-External>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Authen::CAS::External
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Authen-CAS-External>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Authen-CAS-External>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Authen-CAS-External>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Authen-CAS-External/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Douglas Christopher Wilson, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+
+=cut
+
+1; # End of Authen::CAS::External
